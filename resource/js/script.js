3d8096d5a8590dcb5fe1994593e6d89fdb5f9f69
xinranyu1108 <xinranyu1108@outlook.com>
2019-03-03 17:11:53 +1300

    fix navigation bug

--- resource/js/script.js
+++ resource/js/script.js
@@ -10,4 +10,30 @@ $(document).ready(function () {
     }, {
         offset: '70px;'
     });
+
+    $('.js--aboutMe-section').waypoint(function (direction) {
+        if (direction == "down") {
+            $('.header-text-box').addClass('sticky');
+        } else {
+            $('.header-text-box').removeClass('sticky');
+        }
+
+    }, {
+        offset: '70px;'
+    });
+
+    $('.mini-nav-icon').click(function () {
+        var icon = $('.mini-nav-icon i');
+        $('nav').slideToggle(200);
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+
+        }
+
+    })
 });
